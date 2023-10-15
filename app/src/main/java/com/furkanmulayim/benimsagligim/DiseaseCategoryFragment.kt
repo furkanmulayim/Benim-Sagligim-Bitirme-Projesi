ea57793edace6bc33797c2c5894e33148b93bb92
furkanmulayim <furkanmulayim80@gmail.com>
2023-10-15 14:00:03 +0300

    Bana Özel Sayfası Kullanıcı Arayüzü Tamamlandı.

--- app/src/main/java/com/furkanmulayim/benimsagligim/DiseaseCategoryFragment.kt
+++ app/src/main/java/com/furkanmulayim/benimsagligim/DiseaseCategoryFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.furkanmulayim.benimsagligim.databinding.FragmentDiseaseCategoryBinding
 
 class DiseaseCategoryFragment : Fragment() {
@@ -16,9 +17,18 @@ class DiseaseCategoryFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_disease_category, container, false)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_disease_category, container, false)
+        backButton()
         return binding.root
+    }
+
+    private fun backButton() {
+        binding.backButton.setOnClickListener {
+            Navigation.findNavController(requireView())
+                .navigate(R.id.action_diseaseCategoryFragment_to_homeFragment)
+        }
     }
 
 }
